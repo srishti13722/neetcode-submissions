b465648d21e6aaf18a6886017cc8cf5e3bef028a
srishti13722 <95122789+srishti13722@users.noreply.github.com>
2026-06-16 11:34:30 +0530

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.go
@@ -0,0 +1,41 @@
+func helper(i , j , idx int, board [][]byte, word string) bool{
+	if idx == len(word)-1{
+		return true
+	}
+
+	dx := []int{-1, 0, 0, 1}
+	dy := []int{0, -1, 1, 0}
+
+	original := board[i][j]
+	board[i][j] = '#'
+
+	for k := 0 ; k < 4; k++{
+		r := i + dx[k]
+		c := j + dy[k]
+
+		if r >=0 && r < len(board) && c >= 0 && c < len(board[0]) && board[r][c] == word[idx+1]{
+			if helper(r, c, idx+1, board, word){
+				return true
+			}
+		}
+	}
+
+	board[i][j] = original
+	return false
+}
+func exist(board [][]byte, word string) bool {
+	n := len(board)
+	m := len(board[0])
+	for i := 0 ; i < n ; i++{
+		for j := 0 ; j < m ; j++{
+			if board[i][j] == word[0]{
+				if helper(i, j, 0 , board, word){
+					return true
+				}
+			}
+		}
+	}
+
+	return false
+
+}
